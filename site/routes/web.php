60c25a2f0ee4e97d04388a419b0450b858a6aa94
Shakil Ahmed <shakilatrai5@gmail.com>
2020-09-28 19:43:26 +0600

    Laravel course web dev start

--- site/routes/web.php
+++ site/routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+Route::get('/', 'HomeController@HomeIndex');
+Route::post('/contactSend', 'HomeController@ContactSend');
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/Courses', 'CoursesController@CoursePage');
+Route::get('/Projects', 'ProjectsController@ProjectPage');
+Route::get('/Policy', 'PolicyController@PolicyPage');
+Route::get('/Terms', 'TermsController@TermPage');
+Route::get('/Contact', 'ContactController@ContactPage');
